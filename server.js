2c4dddc0cf972536fc5dd63c7d336e1f679f1dc0
Roniwilliams <135303397+Roni1256@users.noreply.github.com>
2024-11-05 21:53:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT =process.env.PORT || 5000;
 
 app.use(cors(
     {
-        origin:'http://localhost:3000',
+        origin:'https://billing-dusky.vercel.app/',
         methods:'GET,POST,PUT,DELETE,PATCH',
         credentials:true
     }
