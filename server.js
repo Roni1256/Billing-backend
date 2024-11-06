3b19bc8ebeedfb3b81996228d7465e42541690a6
Roniwilliams <ronideni1220@gmail.com>
2024-11-06 09:35:53 +0530

    ignore

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT =process.env.PORT || 5000;
 
 app.use(cors(
     {
-        origin:'https://billing-dusky.vercel.app/',
+        origin:["*"],
         methods:'GET,POST,PUT,DELETE,PATCH',
         credentials:true
     }
